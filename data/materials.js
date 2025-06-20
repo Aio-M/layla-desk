c0cd4badee0cc38b21d7e37783c6038a8918c1ce
Aio-M <postapotech@gmail.com>
2025-06-20 22:40:12 +0900

    Update materials.js

--- data/materials.js
+++ data/materials.js
@@ -1,10 +1,10 @@
 const allMaterialsData = {
-  // パイモンが一番好きなもの
+  // 通貨
   "mora": { "name": "モラ", "icon": "images/materials/Mora_icon.webp" },
-
-  // 経験値本
+  
+  // キャラクター経験値素材
   "hero_wit": { "name": "大英雄の経験", "icon": "images/materials/Heros_Wit_icon.webp" },
-
+  
   // 宝石 (Gem)
   "shivada_jade_sliver": { "name": "哀切なアイスクリスタル・砕屑", "icon": "images/materials/gem/Shivada_Jade_Sliver_icon.webp" },
   "shivada_jade_fragment": { "name": "哀切なアイスクリスタル・欠片", "icon": "images/materials/gem/Shivada_Jade_Fragment_icon.webp" },
@@ -14,7 +14,7 @@ const allMaterialsData = {
   // ボス素材 (Boss)
   "perpetual_caliber": { "name": "永続コア", "icon": "images/materials/boss/Perpetual_Caliber_icon.webp" },
 
-  // 地域特産品 (Local Specialty)
+  // 地域特産品 (Local)
   "nilotpala_lotus": { "name": "サウマラタ蓮", "icon": "images/materials/local/Nilotpala_Lotus_icon.webp" },
 
   // コモン素材 (Common)
