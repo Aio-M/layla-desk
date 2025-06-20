824c07c1c1e093cb6a8f972bf5bc590a72d780fa
Aio-M <postapotech@gmail.com>
2025-06-20 23:50:43 +0900

    Update materials.js

--- data/materials.js
+++ data/materials.js
@@ -1,26 +1,45 @@
 const allMaterialsData = {
-  // 通貨
+  // =====パイモンが一番好きなもの=====
   "mora": { "name": "モラ", "icon": "images/materials/Mora_icon.webp" },
   
-  // キャラクター経験値素材
+  // =====経験値本=====
   "hero_wit": { "name": "大英雄の経験", "icon": "images/materials/Heros_Wit_icon.webp" },
   
-  // 宝石 (Gem)
+  // =====宝石 (Gem)=====
+  // 氷
   "shivada_jade_sliver": { "name": "哀切なアイスクリスタル・砕屑", "icon": "images/materials/gem/Shivada_Jade_Sliver_icon.webp" },
   "shivada_jade_fragment": { "name": "哀切なアイスクリスタル・欠片", "icon": "images/materials/gem/Shivada_Jade_Fragment_icon.webp" },
   "shivada_jade_chunk": { "name": "哀切なアイスクリスタル・塊", "icon": "images/materials/gem/Shivada_Jade_Chunk_icon.webp" },
   "shivada_jade_gemstone": { "name": "哀切なアイスクリスタル", "icon": "images/materials/gem/Shivada_Jade_Gemstone_icon.webp" },
 
-  // ボス素材 (Boss)
+  // =====ボス素材 (Boss)=====
+  // スメール
   "perpetual_caliber": { "name": "永続コア", "icon": "images/materials/boss/Perpetual_Caliber_icon.webp" },
 
-  // 地域特産品 (Local)
+  // =====天賦本 (Talent Book)=====
+  // スメール
+  "teachings_of_ingenuity": { "name": "「創意」の教え", "icon": "images/materials/talent/Teachings_of_Ingenuity_icon.webp"},
+  "guide_to_ingenuity": { "name": "「創意」の導き", "icon": "images/materials/talent/Guide_to_Ingenuity_icon.webp"},
+  "philosophies_of_ingenuity": { "name": "「創意」の哲学", "icon": "images/materials/talent/Philosophies_of_Ingenuity_icon.webp"},
+
+  // =====週ボス素材 (Weekly Boss)=====
+  // 「正機の神」 [七葉寂照秘密主]
+  "dakas_bell": { "name": "傀儡の紐", "icon": "images/materials/weekly_boss/Dakas_Bell_icon.webp"},
+  "puppet_strings": { "name": "無心の淵鏡", "icon": "images/materials/weekly_boss/Puppet_Strings_icon.webp"}
+    
+  // =====知恵の冠=====
+  "crown_of_insight": { "name": "知恵の冠", "icon": "images/materials/Crown_of_Insight_icon.webp"},
+
+  // =====地域特産品 (Local)=====
+  // スメール
   "nilotpala_lotus": { "name": "サウマラタ蓮", "icon": "images/materials/local/Nilotpala_Lotus_icon.webp" },
 
-  // コモン素材 (Common)
+  // =====コモン素材 (Common)=====
+  // キノコン
   "fungal_spores": { "name": "キノコンの胞子", "icon": "images/materials/common/Fungal_Spores_icon.webp" },
   "luminescent_pollen": { "name": "キノコンの蛍胞子", "icon": "images/materials/common/Luminescent_Pollen_icon.webp" },
   "crystalline_cyst_dust": { "name": "キノコンの晶塵", "icon": "images/materials/common/Crystalline_Cyst_Dust_icon.webp" },
+  // ヒルチャールシャーマン
   "divining_scroll": { "name": "占いの絵巻", "icon": "images/materials/common/Divining_Scroll_icon.webp" },
   "sealed_scroll": { "name": "封魔の絵巻", "icon": "images/materials/common/Sealed_Scroll_icon.webp" },
   "forbidden_curse_scroll": { "name": "禁呪の絵巻", "icon": "images/materials/common/Forbidden_Curse_Scroll_icon.webp" }
