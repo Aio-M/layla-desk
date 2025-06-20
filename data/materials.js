19a41e59a9172bc415b9af4f3bf9584d9a03d5bb
Aio-M <postapotech@gmail.com>
2025-06-20 20:08:27 +0900

    Create materials.js

---
+++ data/materials.js
@@ -0,0 +1,27 @@
+const allMaterialsData = {
+  // パイモンが一番好きなもの
+  "mora": { "name": "モラ", "icon": "images/materials/mora.webp" },
+
+  // 経験値素材
+  // (今後のための準備)
+
+  // 宝石 (Gem)
+  "shivada_jade_sliver": { "name": "哀切なアイスクリスタル・砕屑", "icon": "images/materials/gem/shivada_jade_sliver.webp" },
+  "shivada_jade_fragment": { "name": "哀切なアイスクリスタル・欠片", "icon": "images/materials/gem/shivada_jade_fragment.webp" },
+  "shivada_jade_chunk": { "name": "哀切なアイスクリスタル・塊", "icon": "images/materials/gem/shivada_jade_chunk.webp" },
+  "shivada_jade_gemstone": { "name": "哀切なアイスクリスタル", "icon": "images/materials/gem/shivada_jade_gemstone.webp" },
+
+  // ボス素材 (Boss)
+  "perpetual_caliber": { "name": "永続コア", "icon": "images/materials/boss/perpetual_caliber.webp" },
+
+  // 地域特産品 (Local Specialty)
+  "nilotpala_lotus": { "name": "サウマラタ蓮", "icon": "images/materials/local/nilotpala_lotus.webp" },
+
+  // コモン素材 (Common)
+  "fungal_spores": { "name": "キノコンの胞子", "icon": "images/materials/common/fungal_spores.webp" },
+  "luminescent_pollen": { "name": "キノコンの蛍胞子", "icon": "images/materials/common/luminescent_pollen.webp" },
+  "crystalline_cyst_dust": { "name": "キノコンの晶塵", "icon": "images/materials/common/crystalline_cyst_dust.webp" },
+  "divining_scroll": { "name": "占いの絵巻", "icon": "images/materials/common/divining_scroll.webp" },
+  "sealed_scroll": { "name": "封魔の絵巻", "icon": "images/materials/common/sealed_scroll.webp" },
+  "forbidden_curse_scroll": { "name": "禁呪の絵巻", "icon": "images/materials/common/forbidden_curse_scroll.webp" }
+};
